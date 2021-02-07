7a796b5a92bf2c067051a58356b4fde5a322f440
Tuan Huynh <thuynh71@gatech.edu>
2021-02-06 20:26:33 -0500

    complete update api

--- back_end/todo_list/views.py
+++ back_end/todo_list/views.py
@@ -4,6 +4,7 @@ from .serializers import TaskSerializer
 from django.contrib.auth.models import User
 import json
 from rest_framework import status
+from .models import Task
 
 @api_view(['POST'])
 def create_task_view(request):
@@ -14,7 +15,6 @@ def create_task_view(request):
     serializer = TaskSerializer(data=request.data)
     if serializer.is_valid():
         user = User.objects.get(username=username)
-        print('here')
         task = serializer.save(user=user)
         Response(serializer.data, status=200)
     return Response({}, status=400)
@@ -28,7 +28,7 @@ def get_tasks_view(request):
     # get user
     user = User.objects.get(username=username)
     # get tasks based on user
-    tasks = user.task_set.all()
+    tasks = user.tasks.all()
     serializer = TaskSerializer(tasks, many=True)
     return Response(serializer.data)
 
@@ -38,4 +38,13 @@ def delete_task_view(request):
 
 @api_view(['POST'])
 def update_task_view(request):
-    return Response({'hello': 'world'})
+    # get param
+    body_unicode = request.body.decode('utf-8')
+    request_params = json.loads(body_unicode)
+    task_id = request_params['task_id']
+    new_title = request_params['new_title']
+    # get task
+    task = Task.objects.get(id=task_id)
+    task.title = new_title
+    task.save()
+    return Response({'messages': 'update success'}, status=200)
