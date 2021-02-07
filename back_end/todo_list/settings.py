573d47452a385bbeca0c133ea74d1cd63b8c2c42
Tuan Huynh <thuynh71@gatech.edu>
2021-02-07 13:58:35 -0500

    add email_backend settings

--- back_end/todo_list/settings.py
+++ back_end/todo_list/settings.py
@@ -53,7 +53,7 @@ INSTALLED_APPS = [
     'dj_rest_auth.registration',
 ]
 SITE_ID = 1
-
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
